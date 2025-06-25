3ad290692dd6e23db82c5804928016a2f3c2f954
zeinputraa <kurniaadika5@gmail.com>
2025-06-25 09:37:43 +0700

    Remove .venv from repo and add to .gitignore

--- .venv/lib/python3.10/site-packages/streamlit/static/static/js/mergeWith.DzwwH6AG.js
+++
@@ -1,1 +0,0 @@
-import{aP as u,aQ as m,g as h}from"./index.BYo0ywlm.js";var e,r;function W(){if(r)return e;r=1;var t=u(),a=m(),i=a(function(s,g,n,o){t(s,g,n,o)});return e=i,e}var c=W();const v=h(c);export{v as m};
